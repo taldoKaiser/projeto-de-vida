677936ee7e9d82e3068ab13345269c594cc28b1d
taldoKaiser <111514213+taldoKaiser@users.noreply.github.com>
2025-05-23 08:43:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,27 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+  minutos%= 60;
+  horas%= 24;
+  iff (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0,0,0,0];}
+}
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.lenght; i++) {
+    document.getElementByld("dias" + i).textContent = calculaTempo(tempo[i])[0];
+    document.getElementByld("horas" + i).textContent = calculaTempo(tempo[i])[1];
+    document.getElementByld("min" + i).textComent = calculaTempo(tempo{i})[2];
+    document.getElementByld("seg" + i).textComent = calculaTempo(tempo[i])[3];
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
